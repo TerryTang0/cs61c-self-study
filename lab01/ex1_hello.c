25550ff6539a4f18467ae865044d53414199619a
Terry Tang <tyz2425769949@gmail.com>
2026-04-17 00:18:53 -0700

    Finished Lab 01 (pointers, arrays, strings, structs)

---
+++ lab01/ex1_hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello 61C\n");
+    return 0;
+}
---
+++ lab01/lab01.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, CS61C Lab 1!\n");
+    return 0;
+}
