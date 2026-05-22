ac457ca1d6c4712eaab43be0a8f02335d457c124
Terry Tang <tyz2425769949@gmail.com>
2026-05-22 00:42:23 -0700

    Finished Lab04 - RISC-V calling convention

--- lab04/ex3.s
+++ lab04/ex3.s
@@ -39,8 +39,18 @@ ex3:
     # otherwise, return ex3(a0, a1-1) * a0
     mv t0 a0      # save a0 in t0
     addi a1 a1 -1 # decrement a1
+    
+    # Set stack pointer for calling convention
+    addi sp sp -8
+    sw t0 0(sp)
+    sw ra 4(sp)
 
     jal ra ex3    # call ex3(a0, a1-1)
+    
+    # Retrieve t0 and ra from the stack pointer
+    lw t0 0(sp)
+    lw ra 4(sp)
+    addi sp sp 8
 
     mul a0 a0 t0  # multiply ex3(a0, a1-1) by t0
                   # (which contains the value of a0)
