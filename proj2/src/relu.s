25e1fcd081e3ae056a3e0380cea9e8fca718759d
Terry Tang <tyz2425769949@gmail.com>
2026-05-29 22:50:36 -0700

    Finished Task 2 - ReLU, Passed the 3 debuggers and the bash testing

--- proj2/src/relu.s
+++ proj2/src/relu.s
@@ -14,25 +14,39 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    mv t0, a0    # Copy the start of array to register t0
+    mv t1, a1    # Copy the length of the array to register t1
+    li t2, 0    # Initialize the counter to 0
+    li t3, 1    # t3 = 1
+    blt a1, t3, loop_error
 
 loop_start:
-
-
-
-
+    bge t2, a1, loop_end
+    lw t4 0(t0)    # t4 = arr[i]
+    bge t4, x0, loop_continue
+    sw x0, 0(t0)    # set the negative value to 0
 
 
 
 
 loop_continue:
+    addi t0, t0, 4
+    addi t2, t2, 1
+    j loop_start
 
+
+loop_error:
+    li a0, 36
+    j exit
 
 
 loop_end:
 
 
     # Epilogue
-
+    lw ra, 0(sp)
+    addi sp, sp, 4
 
     jr ra
