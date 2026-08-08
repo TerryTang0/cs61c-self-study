fca3bf85b5a200fb4458c8ea4ab3fc3577271320
Terry Tang <tyz2425769949@gmail.com>
2026-08-08 02:21:35 -0700

    Task 9 work in progress - just read 3 matrices

--- proj2/src/classify.s
+++ proj2/src/classify.s
@@ -24,13 +24,43 @@
 # Usage:
 #   main.s <M0_PATH> <M1_PATH> <INPUT_PATH> <OUTPUT_PATH>
 classify:
+
+    # Prologue
+    addi sp, sp, 
+
+    mv s0, a0   # s0 = a0
+    mv s1, a1   # s1 = a0; s1 stores the pointer to an array of argument strings
+
     # Read pretrained m0
+    lw a0, 4(s1)    # Load pointer to the filepath of m0 to a0
+    addi sp, sp, -20
+    addi a1, sp, 0
+    addi a2, sp, 4
+
+    jal ra, read_matrix
+
+    mv s2, a0   # s2 = a0; s2 store the pointer to m0 in the memory
 
 
     # Read pretrained m1
+    lw a0, 8(s1)    #Load pointer to the file path of m1 to a0
+    addi a1, sp, 8
+    addi a2, sp, 12
+
+    jal ra, read_matrix
+
+    mv s3, a0   # s3 = a0; s3 stores the pointer to m1 in the memory
+
 
 
     # Read input matrix
+    lw a0, 12(s1)    #Load pointer to the file path of m1 to a0
+    addi a1, sp, 16
+    addi a2, sp, 20
+
+    jal ra, read_matrix
+
+    mv s4, a0   # s4 = a0; s4 stores the pointer to m1 in the memory
 
 
     # Compute h = matmul(m0, input)
