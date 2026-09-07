81f58b3e8e2313cd6124bd3a829d61e41bf90e22
Terry Tang <tyz2425769949@gmail.com>
2026-09-06 23:56:51 -0700

    Task 5 WIP - not pass my written test. there is bug in control logic circuit

---
+++ proj3/tests/integration-custom/in/itype_test.s
@@ -0,0 +1,14 @@
+
+li x1, 6
+li x2, -42
+
+addi x3, x1, 4      # x3 = x1 + 4 = 10
+slli x4, x1, 2      # x4 = x1 * 4 = 24
+slti x5, x1, 7      # x5 = 1
+slti x6, x1, 5      # x6 = 0
+xori x7, x1, -1     # x7 = -7
+srli x8, x1, 2      # x8 = 1
+srli x9, x2, 2      # x9 = 53
+srai x10, x2, 2     # x10 = 107
+ori x11, x0, 5      # x11 = 5
+
